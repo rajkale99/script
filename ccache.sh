b4a7ef21b9d173e28345347c18454bad7d6f9f11
Rajkale99 <kaleraj.rk@gmail.com>
2018-10-05 09:40:26 +0000

    Added ak env script

---
+++ ccache.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd /tmp || exit 1
+git clone git://github.com/akhilnarang/ccache.git
+cd ccache || exit 1
+./autogen.sh
+./configure
+make -j"$(nproc)"
+sudo make install
+rm -rf "${PWD}"
+cd - || exit 1
--- rkrom.sh
+++ rkrom.sh
@@ -30,10 +30,11 @@ sudo apt-get update
 sudo apt-get install openjdk-8-jdk
 sudo apt-get install openjdk-8-jre
 echo -e "$red Install Build Tools"
-sudo apt-get install git-core python gnupg flex bison gperf build-essential zip curl gcc-multilib g++-multilib libc6-dev-i386  ccache libgl1-mesa-dev lib32ncurses5-dev \ unzip libesd0-dev squashfs-tools
-sudo apt-get update && sudo apt-get install bc git-core gnupg flex bison gperf squashfs-tools build-essential zip curl libncurses5-dev openjdk-8-jre openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32ncurses5-dev gcc-multilib maven tmux screen w3m ncftp
- sudo apt-get install git-core gnupg flex bison gpe rf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip curl flex git liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev maven pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev g++-multilib gcc-multilib
 
+sudo apt-get install git-core python gnupg flex bison gperf build-essential zip curl gcc-multilib g++-multilib libc6-dev-i386  ccache libgl1-mesa-dev lib32ncurses5-dev \ unzip libesd0-dev squashfs-tools -y
+sudo apt-get update && sudo apt-get install bc git-core gnupg flex bison gperf squashfs-tools build-essential zip curl libncurses5-dev openjdk-8-jre openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32ncurses5-dev gcc-multilib maven tmux screen w3m ncftp -y
+sudo apt-get install git-core gnupg flex bison gpe rf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip curl flex git liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev maven pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev g++-multilib gcc-multilib wput -y
+sudo apt-get install wget -y
 echo -e "$blue Configure Repo And git"
 
 curl https://storage.googleapis.com/git-repo-downloads/repo > repo
@@ -41,6 +42,17 @@ chmod a+x repo
 sudo install repo /usr/local/bin
 rm repo
 sudo apt-get upgrade -y
-echo -e "$purple Syncing Vince source"
-git clone https://github.com/rajkale99/vendor_xiaomi_vince -b 8.1 vendor/xiaomi
-git clone https://github.com/Blacksuan19/android_kernel_dark_ages -b darky kernel/xiaomi/vince
+
+echo -e "Gdrive Installing"
+
+cd $(pwd)
+
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
+mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
+chmod +x gdrive
+sudo install gdrive /usr/local/bin/gdrive
+
+echo -e "Installed Gdrive"
+
+cd $(pwd)
+source akhil.sh
