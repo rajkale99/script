c3d4db1d66789095b39bbbc6fab07e4bffb67732
Rajkale99 <kaleraj.rk@gmail.com>
2018-11-02 20:09:53 +0000

    Added Pixys and Msm Xtended Bash Script

---
+++ pixys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "$red Bash Script For Cloning Trees Stuff"
+
+#color
+purple='\033[1;35m'
+cyan='\033[0;36m'
+red='\033[0;31m'
+nc='\033[0m'
+
+echo -e "$purple Maintained By Rajkale99"
+
+echo -e "$cyan Cloning Trees"
+git clone https://android.googlesource.com/platform/hardware/qcom/wlan hardware/qcom/wlan
+git clone -b pie https://github.com/rajkale99/device_xiaomi_vince device/xiaomi/vince
+git clone -b pie https://github.com/rajkale99/android_device_xiaomi_msm8953-common device/xiaomi/msm8953-common
+git clone -b pie https://github.com/rajkale99/proprietary_vendor_xiaomi vendor/xiaomi
+git clone -b android-9.0-inline https://github.com/rama982/kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
